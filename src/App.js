01675e7d4511b7df7db41458309c609c5e30bd70
blaec <odeskonst@yandex.ru>
2020-02-28 21:12:03 +0200

    6-Add a 404 error page and render it for any unknown routes

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
                 <Switch>
                     <Route path="/users" component={Users}/>
                     <Route path="/courses" component={Courses}/>
+                    <Route render={() => <h1>Not found</h1>}/>
                     {/*<Route path="/courses/:id" component={Course}/>*/}
                 </Switch>
             </div>
